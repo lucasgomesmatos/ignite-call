1399087483674cd71dc4fa4d565a9d6521081b35
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-06-04 16:20:40 -0300

    config storybook

--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -1,8 +1,7 @@
-/// <reference types="vite/client" />
-
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+/// <reference types="vite/client"/>
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-});
+})
